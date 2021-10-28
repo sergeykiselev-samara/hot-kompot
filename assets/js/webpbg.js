b20e353f4fb3a5976ef8f9f65b37680ed9809c56
sergeykiselev-samara <66842866+sergeykiselev-samara@users.noreply.github.com>
2021-10-28 21:20:56 +0400

    new site

---
+++ assets/js/webpbg.js
@@ -0,0 +1,36 @@
+
+// Проверяем, можно ли использовать Webp формат
+function canUseWebp() {
+    // Создаем элемент canvas
+    let elem = document.createElement('canvas');
+    // Приводим элемент к булеву типу
+    if (!!(elem.getContext && elem.getContext('2d'))) {
+        // Создаем изображение в формате webp, возвращаем индекс искомого элемента и сразу же проверяем его
+        return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
+    }
+    // Иначе Webp не используем
+    return false;
+}
+window.onload = function () {
+    // Получаем все элементы с дата-атрибутом data-bg
+    let images = document.querySelectorAll('[data-bg]');
+    // Проходимся по каждому
+    for (let i = 0; i < images.length; i++) {
+        // Получаем значение каждого дата-атрибута
+        let image = images[i].getAttribute('data-bg');
+        // Каждому найденному элементу задаем свойство background-image с изображение формата jpg
+        images[i].style.backgroundImage = 'url(' + image + ')';
+    }
+    // Проверяем, является ли браузер посетителя сайта Firefox и получаем его версию
+    let isitFirefox = window.navigator.userAgent.match(/Firefox\/([0-9]+)\./);
+    let firefoxVer = isitFirefox ? parseInt(isitFirefox[1]) : 0;
+    // Если есть поддержка Webp или браузер Firefox версии больше или равно 65
+    if (canUseWebp() || firefoxVer >= 65) {
+        // Делаем все то же самое что и для jpg, но уже для изображений формата Webp
+        let imagesWebp = document.querySelectorAll('[data-bg-webp]');
+        for (let i = 0; i < imagesWebp.length; i++) {
+            let imageWebp = imagesWebp[i].getAttribute('data-bg-webp');
+            imagesWebp[i].style.backgroundImage = 'url(' + imageWebp + ')';
+        }
+    }
+};
